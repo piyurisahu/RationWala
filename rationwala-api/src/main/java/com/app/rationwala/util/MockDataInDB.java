c45539ca199872220f528846a4f88c0c62c54d50
piyurisahu <piyurisahu@gmail.com>
2021-01-03 14:16:49 -0500

    [api]:- Added url in seller

--- rationwala-api/src/main/java/com/app/rationwala/util/MockDataInDB.java
+++ rationwala-api/src/main/java/com/app/rationwala/util/MockDataInDB.java
@@ -23,52 +23,63 @@ public class MockDataInDB {
 		// Seller profiles
 		repo.save(new UserProfile(new UserLogin("Seller1", "admin"), "Tarun",
 				"Gupta", "tarung@abc.com" , "9494942354", "XYZ Gomti Nagar",
-				"Lucknow", "12345", true, "Gupta General Store", ""));
+				"Lucknow", "12345", true, "Gupta General Store",
+				"https://image.winudf.com/v2/image/Y29tLmFwcGxvcC5yYWplc2hndXB0YTg4OTIxODc2MDg1Ml8zMTk1MTBfc2NyZWVuc2hvdHNfMV9iNjkzMDliYQ/screen-1.jpg?fakeurl=1&type=.jpg"));
 
 		repo.save(new UserProfile(new UserLogin("Seller2", "admin"), "Kiran",
 				"Arora", "kirana@abc.com" , "9494987654", "5/7 Vinay Khand",
-				"Lucknow", "12365", true, "Arora Departmental Store", ""));
+				"Lucknow", "12365", true, "Arora Departmental Store",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_c_JNmyN0j9fVy9m7IV_CQmkos1W8DiTjg&usqp=CAU"));
 
 		repo.save(new UserProfile(new UserLogin("Seller3", "admin"), "Vikas",
 				"Malik", "vikasM@abc.com" , "9484987654", "55 Shaheed ChandraShekhar Chowk",
-				"Gomti Nagar, Lucknow", "226010", true, "Mr brown Ration Mall", ""));
+				"Gomti Nagar, Lucknow", "226010", true, "Mr brown Ration Mall",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjMnam8IlGxLPKbwkX-Zw9hCoWdpZow3uDUw&usqp=CAU"));
 
 
 		repo.save(new UserProfile(new UserLogin("Seller4", "admin"), "Tanushree",
 				"Gupta", "TanuSh@abc.com" , "9484987772", "67/5 Indira Nagar ",
-				"Sector 14, Lucknow", "226011", true, "1 Stop Grocery", ""));
+				"Sector 14, Lucknow", "226011", true, "1 Stop Grocery",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR5L1WsZI0CJqnaQZ4yi4cvWDmuW_RypHxRA&usqp=CAU"));
 
 
 		repo.save(new UserProfile(new UserLogin("Seller5", "admin"), "Sonu",
 				"Khan", "SonuK@abc.com" , "9484828200", "Shop no 123, metro Plaza",
-				"Metro station, Lucknow", "226016", true, "EveryDay Mart ", ""));
+				"Metro station, Lucknow", "226016", true, "EveryDay Mart ",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-x9aYHLiRPUn8Oua4_XcAdEkkr-34ibpfew&usqp=CAU"));
+
 
 
 		repo.save(new UserProfile(new UserLogin("Seller6", "admin"), "Rakesh",
 				"Yadav", "rakeshY@abc.com" , "9484828772", "C-1/2, Sahara State Road",
-				"Sector G, Lucknow", "226021", true, "GrocerSpace ", ""));
+				"Sector G, Lucknow", "226021", true, "Easyday Club ",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGCsWjT2zpTF083A6p819zwMBCdzN2ZwNH5g&usqp=CAU"));
 
 
 		repo.save(new UserProfile(new UserLogin("Seller7", "admin"), "Raman",
 				"Sahu", "ramanS@abc.com" , "9484833772", "6/9, Nayak Nagar",
-				"Sitapur Road, Lucknow", "226021", true, "Day to Day mart", ""));
+				"Sitapur Road, Lucknow", "226021", true, "Day to Day mart",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqdcS0orxAoeHhsA26MxrquoVrhljf4wTGRg&usqp=CAU"));
 
 
 		repo.save(new UserProfile(new UserLogin("Seller8", "admin"), "Kapil",
 				"Mishra", "kapilM@abc.com" , "9484833000", "Ring Road, Near Cocacola Goadam",
-				"Vikas Nagar, Lucknow", "226022", true, "Family Bazar", ""));
+				"Vikas Nagar, Lucknow", "226022", true, "Family Bazar",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQexHrpxpKEcbPH9T33GzzyH6RbBpg1TyXBQQ&usqp=CAU"));
 
 
 
 		repo.save(new UserProfile(new UserLogin("Seller9", "admin"), "Anjali",
 				"Tripathi", "anjaliT@abc.com" , "94849468730", "3/48, Mansarovar Yojna",
-				"Jalvayu Vihar Phase 2, Lucknow", "226010", true, "All Season Fresh Mart", ""));
+				"Jalvayu Vihar Phase 2, Lucknow", "226010", true, "All Season Fresh Mart",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0P7Vh_j-jcmtVmdMl7DlicIqNmIynUN_Fkw&usqp=CAU"));
 
 
 
 		repo.save(new UserProfile(new UserLogin("Seller10", "admin"), "Lucky",
 				"Sachdeva", "luckyS@abc.com" , "94849473777", "HG-94, LDA Colony",
-				"Kanpur Road, Lucknow", "226012", true, "Sachdeva Store", ""));
+				"Kanpur Road, Lucknow", "226012", true, "Sachdeva Store",
+				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTliNOeoaee5Ec3JRcf5ElPdeg7YiI8ir9tZQ&usqp=CAU"));
 
 		// User profiles
 		IntStream.range(1, 11)
